dfc21f9f87e51e374bf6f5f0a39ee236bdabec0c
SaturnDraco61 <hardbadaschanel@gmail.com>
2020-10-27 21:42:16 -0300

    Editing

--- src/commands/rulet01.js
+++ src/commands/rulet01.js
@@ -3,8 +3,9 @@ const MessageEmbed = require("discord.js").MessageEmbed;
 const execute = (bot, msg, args) => {
     const embed = new MessageEmbed()
     .setColor("0x410000")
-    .setTitle('Regras do server - Administração;')
-    .setDescription("Leia atentamente as regras do servidor e as siga, caso contrário você será expulso do torneio!")
+    .setTitle('Regulamento do torneio!')
+    .setDescription("Regras escritas por Robin e YongFre")
+    .setThumbnail('https://i.pinimg.com/originals/e9/51/c9/e951c992214c77307af860f7dc7db5c3.gif')
     .addFields(
         { name: 'Note - Regras do Jogador ➥', value: 'Os jogadores devem ter o espírito esportivo sempre, ou seja, todos devem seguir as regras estabelecidas pela própria desenvolvedora do jogo, Riot Games'},
         { name: '(1) ➥', value: 'Trate os oficiais do torneio, os outros jogadores e os espectadores com respeito  tenha espírito esportivo durante todo o evento.' },
